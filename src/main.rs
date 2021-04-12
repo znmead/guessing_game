28add4fa95547fe06a542bfb8a352b0215e95b06
Zach Mead <zachary.n.mead@gmail.com>
2021-04-12 13:14:04 -0500

    Secret number

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
+    println!("The secret number is {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
